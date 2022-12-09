d6cc4cfc1cc68934acf93f13e3dfdd439eff9fca
Michael <mklassen4124@gmail.com>
2022-12-09 08:44:22 -0800

    Implement event handler on rock button

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+/*const buttons = Array.from(document.querySelectorAll('button'));
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            playerSelection = button.id.toString();
+            console.log(playerSelection);
+        });
+    });
+
+window.addEventListener('click', game);*/
+
+let playerSelection;
+
+const rockButton = document.querySelector('.rock');
+rockButton.addEventListener('click', e => {
+    playerSelection = e.target.classList.value;
+    console.log(playerSelection);
+    game(playerSelection);
+});
+
 function getComputerChoice() {
     // Stores a random integer from 1 to 3 inclusive in choice
-    const choice = Math.floor(Math.random() * 3) + 1;
+    let choice = Math.floor(Math.random() * 3) + 1;
 
     // Return "Rock", "Paper", or "Scissors" depending on whether choice is 1, 2, or 3, respectively
     switch (choice) {
@@ -15,8 +34,8 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // Convert strings to lowercase so they can be easily compared
-    playerChoice = playerSelection.toLowerCase();
-    computerChoice = computerSelection.toLowerCase();
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
 
     // Stores the result of the round
     let result;
@@ -65,49 +84,44 @@ function playRound(playerSelection, computerSelection) {
         result = "invalid player entry"
     }
 
+    console.log(result);
     return result;
 }
 
-function game() {
+function game(playerChoice) {
     
     let playerScore = 0;
     let computerScore = 0;
     
-    for (let i = 0; i < 5; i++) {
-        // Prompt player for their choice and store it in playerSelection
-        const playerSelection = prompt("Let's play a game! \nType 'rock', 'paper', or 'scissors' and press Enter.");
-        // Play a round and store the result of the game in result
-        const result = playRound(playerSelection, getComputerChoice());
-        // Stores the text that tells player the result of the round
-        let resultText;
+    let computerChoice = getComputerChoice();
+    let result = playRound(playerChoice, computerChoice);
+    let resultText;
 
-        // Print an alert message that depends on the result of the round and increment player or computer score accordingly
-        switch (result) {
-            case "win":
-                playerScore++;
-                resultText = "You win this round, " + playerChoice + " beats " + computerChoice;
-                break;
-            case "lose":
-                computerScore++;
-                resultText = "You lose this round, " + computerChoice + " beats " + playerChoice;
-                break;
-            case "tie":
-                resultText = "This round is a tie, you both chose " + playerChoice;
-                break;
-            default:
-                resultText = "Invalid entery";
-                // If player enters something invalid, don't progress to the next round
-                i--;
-        }
-
-        let round = i + 1;
-        // Stores text containing the round number
-        let roundText = ". \nRound: " + round;
-        // Stores text containing the score
-        let scoreText = "\nScore: Player - " + playerScore + "  Computer - " + computerScore;
-        // Print an alert containing the result of the round, the round number, and the score
-        alert(resultText + roundText + scoreText);
+    // Print an alert message that depends on the result of the round and increment player or computer score accordingly
+    switch (result) {
+        case "win":
+            playerScore++;
+            resultText = "You win this round, " + playerChoice + " beats " + computerChoice;
+            break;
+        case "lose":
+            computerScore++;
+            resultText = "You lose this round, " + computerChoice + " beats " + playerChoice;
+            break;
+        case "tie":
+            resultText = "This round is a tie, you both chose " + playerChoice;
+            break;
+        default:
+            resultText = "Invalid entry";
+            // If player enters something invalid, don't progress to the next round
     }
+
+    let round = 1;
+    // Stores text containing the round number
+    let roundText = ". \nRound: " + round;
+    // Stores text containing the score
+    let scoreText = "\nScore: Player - " + playerScore + "  Computer - " + computerScore;
+    // Print an alert containing the result of the round, the round number, and the score
+    console.log(resultText + roundText + scoreText);
 
     // Function returns a string declaring the outcome of the game after 5 rounds
     if (playerScore > computerScore) {
@@ -120,5 +134,3 @@ function game() {
         return "It's a tie game."
     }
 }
-
-alert(game());
